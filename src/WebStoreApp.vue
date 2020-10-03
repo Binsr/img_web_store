eb33ad057f618daf5a962b48fb9baa1992d16779
Binsr <urosjovanovic.sna@gmail.com>
2020-10-03 12:02:14 +0200

    stup

--- src/WebStoreApp.vue
+++ src/WebStoreApp.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <div style="background-color:black; height: 100px; width: 100vw"></div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 export default {
---
+++ src/components/basic_layout/MainMenu.vue
@@ -0,0 +1,5 @@
+ <template>
+    <div style="background-color:black; height: 100px; width: 100vw"></div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">Creative</router-link>
+</template>
