e7cde84ba5655f5ae03d84b52199887143e7e332
Binsr <urosjovanovic.sna@gmail.com>
2020-10-03 11:42:52 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,27 @@
   }
 </script>
 <style>
+
+html{
+  padding: 0;
+
+}
+
+body{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0 !important;
+  margin: 0 !important;
 }
 
-#nav {
-  padding: 30px;
-}
+#nav {}
 
 #nav a {
   font-weight: bold;
