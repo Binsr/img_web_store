6e949fee60542ebd421ad31a542f383347d9c43d
Binsr <urosjovanovic.sna@gmail.com>
2020-11-16 22:59:14 +0100

    More styling

--- src/views/Login/SignUpForm.vue
+++ src/views/Login/SignUpForm.vue
@@ -1,9 +1,10 @@
 <template>
       <div class="panel">
-        <div class="input"><div class="textHolder">{{$store.state.msg.username}}</div></div>
-        <div class="input"><div class="textHolder">{{$store.state.msg.password}}</div></div>
-                <div class="input"><div class="textHolder">confirm password</div></div>
+        <div class="input"><div class="textHolder">first name</div></div>
+        <div class="input"><div class="textHolder">last name</div></div>
         <div class="input"><div class="textHolder">e-mail</div></div>
+        <div class="input"><div class="textHolder">password</div></div>
+        <div class="basic-text">Yes i want emails with visual inspiration, special offers and more.</div>
       </div>
 </template>
 <script>
@@ -25,10 +26,15 @@
 
 }
 
+.basic-text{
+    color: #000 !important;
+    width: 300px;
+}
+
 .textHolder{
     margin: auto;
     display: flex;
-    width: min-content;
+    width: max-content;
 }
 
 .input{
